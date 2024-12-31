1fa676abf9f58090ddd54b943d0534d5c032c6b7
Dilip-Dob-1989 <119772054+Dilip-Dob-1989@users.noreply.github.com>
2024-12-31 17:10:43 +0530

    Add files via upload

---
+++ Sentiment_Project.py
@@ -0,0 +1,34 @@
+import streamlit as st
+from textblob import TextBlob
+st.sidebar.title("About Me")
+st.sidebar.text(""" I am Dilip Prajapati
+a Data science Student from
+Ducat institute Noida
+""")
+
+
+st.sidebar.title("Contact Me")
+st.sidebar.text(""" Name: Dilip Prajapati
+Course :Data science & Machine learning
+Mob : 8745024207
+Email :dilip.prajapati.ichak@gmail.com
+""")
+
+
+st.title("Sentiment Analysis Project")
+
+text=st.text_input("**enter text**")
+btn=st.button("Predict")
+
+if btn:
+    blob=TextBlob(text)
+    sent=blob.sentiment[0]
+    if sent<0:
+        st.error("Negative Sentiment")
+        st.image("Sentiment_images/neg_senti.jpg")
+    elif sent>0:
+        st.success("Positive Sentiment")
+        st.image("Sentiment_images/pos_senti.jpg")
+    else:
+        st.warning("Neutral Sentiment")
+        st.image("Sentiment_images/neutral_senti.jpg")
